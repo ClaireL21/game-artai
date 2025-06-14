41722be28366d3020db4b32622d156ac9b9bf58c
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-06-14 11:36:50 -0400

    working on bezier

---
+++ Game-ArtAI/Assets/Point_Viz.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+
+public class Point_Viz : MonoBehaviour
+{
+    private Vector3 mOffset; 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        mOffset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+    }
+
+    private void OnMouseDrag()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        Vector3 curScreenPoint = new Vector3(
+              Input.mousePosition.x,
+              Input.mousePosition.y, 0.0f);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + mOffset;
+        transform.position = curPosition;
+    }
+
+    void OnMouseUp()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+    }
+
+}
