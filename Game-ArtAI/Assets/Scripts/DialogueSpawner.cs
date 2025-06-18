f53244b972cf436c424e7ac2c92b09ab1a835969
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-06-17 21:19:13 -0400

    combining re & jigsaw

--- Game-ArtAI/Assets/Scripts/DialogueSpawner.cs
+++ Game-ArtAI/Assets/Scripts/DialogueSpawner.cs
@@ -36,25 +36,6 @@ public class DialogueSpawner : MonoBehaviour
     void OnReturn(WorldSpaceUI uiDoc) => uiDoc.gameObject.SetActive(false);
     void OnDestroyObj(WorldSpaceUI uiDoc) => Destroy(uiDoc.gameObject);
 
-    public void SpawnDialogue(List<int> items, Vector3 worldPos)
-    {
-        Vector3 spawnPos = worldPos;
-
-        WorldSpaceUI instance = uiDocumentPool.Get();
-       // instance.transform.SetPositionAndRotation(spawnPos, Camera.main.transform.rotation);
-        instance.transform.position = spawnPos;
-        instance.transform.localScale = new Vector3(2.0f, 2.0f, 0.0f);
-
-        this.gameObject.transform.SetPositionAndRotation(spawnPos, Camera.main.transform.rotation);
-
-        // turn items into respective emojis
-        // gotta rename the emojis based on our names
-
-        // <sprite="emojiAsset" index=4>
-
-        instance.SetLabelText(k_labelName, "Hello! <sprite=\"emojiAsset\" index=4>");
-    }
-
     public void SpawnDialogue(Tuple<int, int> items, Vector3 worldPos)
     {
         Vector3 spawnPos = worldPos;
