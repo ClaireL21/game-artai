afd37a8f9a6b2eeb371de0106fc989e82b3e2de4
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-06-17 20:56:59 -0400

    finished base

---
+++ Game-ArtAI/Assets/Scripts/Requests.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using System.Linq;
+
+public class Requests : MonoBehaviour
+{
+    // requests in sprite sheets
+    public static int numPatterns; 
+    public static int numColors;
+
+    // gameobjects
+    [SerializeField] GameObject artworks;
+    [SerializeField] GameObject target;
+
+    // private vars
+    float timer;
+    public bool notActive = true; // should alter this 
+
+    // pick a request every x seconds 
+    // pick random color, pattern combo 
+
+    // make sure color & pattern either exists in scene
+    // or make character w/o art take it 
+    // if not possible reroll
+    // or more efficient -- can only pick available ones?
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        numPatterns = 10;
+        numColors = 10;
+
+        timer = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer -= Time.deltaTime;
+
+        //if (timer <= 0)
+        //{
+        //    // req logic
+
+        //    timer = 0;
+        //    notActive = true;
+        //}
+
+        //if (notActive)
+        //{
+        //    SelectRequest();
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SelectRequest();
+        }
+
+    }
+
+    void SelectRequest()
+    {
+        List<int> requestInfos = new List<int>();
+
+        var artTotal = artworks.transform.childCount;
+
+        for (int i = 0; i < artTotal / 2; i++)
+        {
+            requestInfos.Add(UnityEngine.Random.Range(0, numColors));
+        }
+
+        for (int i = artTotal / 2; i < artTotal; i++)
+        {
+            requestInfos.Add(UnityEngine.Random.Range(0, numPatterns));
+        }
+
+        // pick request & set 
+
+        Tuple<int, int> request = Tuple.Create(requestInfos[0], requestInfos[artTotal / 2]);
+        target.GetComponent<DialoguePicker>().SetDialogue(request);
+
+        requestInfos = requestInfos.OrderBy(x => UnityEngine.Random.value).ToList();
+
+        // set rest of artworks 
+        // IEnumerate? 
+
+        for (int i = 0; i < artTotal; i++)
+        {
+            var child = artworks.transform.GetChild(i);
+            child.GetComponent<Artwork>().Setup(requestInfos[i]); 
+        }
+
+        timer = 5.0f;
+        notActive = false;
+    }
+
+
+}
