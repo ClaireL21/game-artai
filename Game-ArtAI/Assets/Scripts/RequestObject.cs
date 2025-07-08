b37de8b03c13e67d7b12354460ea6a0e1f823247
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-07-08 12:17:43 -0400

    slight changes to add list of materials

--- Game-ArtAI/Assets/Scripts/RequestObject.cs
+++ Game-ArtAI/Assets/Scripts/RequestObject.cs
@@ -7,6 +7,7 @@ public class RequestObject : MonoBehaviour
     private int thirdIndex;     // shape
     private int requestSize;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Game-ArtAI/Assets/Scripts/RequestsManager.cs
+++ Game-ArtAI/Assets/Scripts/RequestsManager.cs
@@ -33,8 +33,10 @@ public class RequestsManager : MonoBehaviour
     //public int currNumRequests;     // number of requests visible in the scene
     public static RequestsManager RM;
 
+    [SerializeField]
+    private Material[] patterns;
 
-
+    
 
     // gameobjects
     //public List<GameObject> requests;
@@ -51,9 +53,9 @@ public class RequestsManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        numColors = 16;
-        numPatterns = 16;
-        numThird = 16;
+        numColors = 12;
+        numPatterns = 12;
+        numThird = 12;
         requests = new Queue<RequestObject>();
         artInfo = new Queue<int>();
 
